0c552198ab2882a9e579e42f6dafc9764b73175e
Arka <itsarkanayan@gmail.com>
2016-04-14 13:37:47 +0530

    Implement edit user form validation

--- app/src/main/java/me/arkanayan/buieconnect/activities/EditUserActivity.java
+++ app/src/main/java/me/arkanayan/buieconnect/activities/EditUserActivity.java
@@ -12,9 +12,18 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.Max;
+import com.mobsandgeeks.saripaar.annotation.Min;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+
 import java.io.IOException;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -30,26 +39,53 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class EditUserActivity extends AppCompatActivity implements View.OnClickListener {
+public class EditUserActivity extends AppCompatActivity implements View.OnClickListener, Validator.ValidationListener {
 
     public final String TAG = this.getClass().getSimpleName();
 
     public static final String EXTRA_AUTH_TOKEN = BuildConfig.APPLICATION_ID + ".extra_auth_token";
     private User mUser;
+    private Validator mEditUserValidator;
 
+    // [Start view bindings]
+    @NotEmpty
     @Bind(R.id.first_name_edit_text)
     TextInputEditText firstNameEditText;
 
+    @NotEmpty
+    @Bind(R.id.edit_text_last_name)
+    TextInputEditText lastNameEditText;
+
+    @NotEmpty
+    @Email
+    @Bind(R.id.edit_text_email)
+    TextInputEditText emailEditText;
+
+    @NotEmpty
+    @Max(2030)
+    @Min(1994)
+    @Bind(R.id.edit_text_passout_year)
+    TextInputEditText passoutYearEditText;
+
+    @NotEmpty
+    @Max(2030)
+    @Min(1994)
+    @Bind(R.id.edit_text_admission_year)
+    TextInputEditText admissionYearEditText;
+
+    @NotEmpty
+    @Bind(R.id.edit_text_univ_roll)
+    TextInputEditText univRollEditText;
+
     @Bind(R.id.layout_input_first_name)
     TextInputLayout firstNameInputLayout;
-    
+    // [End view bindings]
     private EditUserBinding editUserBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         editUserBinding = DataBindingUtil.setContentView(this, R.layout.activity_edit_user);
-
         ActionBar toolbar = getSupportActionBar();
 
         // set click listeners
@@ -57,6 +93,9 @@ public class EditUserActivity extends AppCompatActivity implements View.OnClickL
         //setContentView(R.layout.activity_edit_user);
         ButterKnife.bind(this);
 
+        // Instantiate validator
+        mEditUserValidator = new Validator(this);
+        mEditUserValidator.setValidationListener(this);
         // [ Start user loading/handling ]
 
         Prefs prefs = Prefs.getInstance(this);
@@ -149,10 +188,31 @@ public class EditUserActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button_submit:
+                mEditUserValidator.validate();
                 Log.d(TAG, "onClick submit: firstName " + editUserBinding.firstNameEditText.getText());
                 Log.d(TAG, "onClick submit: is_alumus " + editUserBinding.switchAlumnus.isChecked());
                 Log.d(TAG, "onClick submit: department " + editUserBinding.spinnerDepartment.getSelectedItem().toString());
 
+        }
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        Log.d(TAG, "onValidationSucceeded: Validation Succeed");
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
